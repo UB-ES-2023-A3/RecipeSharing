6e36326db94f5a198d19acf7fbd81c976f3a3a24
gerard2810 <gerardinfo281001@gmail.com>
2023-10-16 23:35:40 +0200

    Login/signin implementados

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 urlpatterns = [
     path("login/", views.login_view),
-    path("login/logged/", views.login_action),
     path("signin/", views.signin_view),
-    path("signin/signedin/", views.signin_action)
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -5,10 +5,6 @@ from django.contrib.auth.models import User
 
 # Create your views here.
 def login_view(request):
-    return render(request, "login.html")
-    # return HttpResponse("Formulario inicio de sesion")
-
-def login_action(request):
     if request.method == "POST" and not request.user.is_authenticated:
         username = request.POST["username"]
         password = request.POST["password"]
@@ -17,25 +13,19 @@ def login_action(request):
         # Check if user exists
         if user is not None:
             login(request=request, user=user)
-
-            return HttpResponse("Pagina de logeo")
-
+            return render(request, "Logout.html", {username: username})
+            # return HttpResponse("Pagina de logeo")
         else:
             return render(request, "login.html")
 
-    else:
-        return HttpResponse("Ya hay un inicio de sesion")
-def logout_action(request):
-    if request.user.is_authenticated:
+    elif request.user.is_authenticated:
         logout(request)
         return HttpResponse("Sesión cerrada correctamente")
 
-    return HttpResponse("No hay iniciada ninguna sesion")
+    else:
+        return render(request, "login.html")
 
 def signin_view(request):
-    return render(request, "register.html")
-
-def signin_action(request):
     if request.method == "POST":
         username = request.POST["username"]
         email = request.POST["email"]
@@ -53,8 +43,6 @@ def signin_action(request):
         # If the user exist, does anything
         return HttpResponse("El usuario ya existe")
 
-    print("NO ENTRA EN LE IF")
-
 # This class is responsible for checking if the email is valid
 class EmailChecker:
     list_valid_emails = ["@gmail.com", "@outlook.com", "@hotmail.com", "@yahoo.com"]
@@ -69,8 +57,27 @@ class EmailChecker:
 class PasswordChecker:
     MAX_LENGTH = 16
     MIN_LENGTH = 8
+    set_simbols = {"!", "@", "#", "~", "%", "&", "/", "(", ")" "=", "*", "+", "-"}
     def check_password(self, password):
-        if self.MIN_LENGTH <= len(password) <= self.MAX_LENGTH:
-            return True
+        # Check the length of password
+        if not self.MIN_LENGTH <= len(password) <= self.MAX_LENGTH:
+            return False
+
+        # Check if exists any number in the password
+        if not any(letter.isDigit() for letter in password):
+            return False
+
+        has_upper = False
+        has_lower = False
+        has_symbol = False
+
+        for letter in password:
+            if letter.isupper():
+                has_upper = True
+            else:
+                has_lower = True
+
+            if has_upper and has_lower:
+                return True
 
         return False
