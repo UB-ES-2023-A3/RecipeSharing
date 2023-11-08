5350e9c7516d6d53727943a2f749cbe88da4ce83
rubensituuh <72190690+rubensituuh@users.noreply.github.com>
2023-11-08 16:19:15 +0100

    Revert "Recipes Query"

--- app/urls.py
+++ app/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('addRecipe/', AddRecipeView.as_view(), name='addRecipe'),
 
     # path('getRecipes/', HomeView.get_recipes, name= 'get_recipes'),
-    path('recipe/<str:query>/', QueryListRecipesView.as_view(), name='queryListRecipes')
+    
     # path('new/', views.create_new_recipe, name='create_new_recipe'),
     # path('recipes/<int:recipe_id>/', views.update_recipe, name='update_recipe'),
     # path('recipes/<int:recipe_id>/', views.delete_recipe, name='delete_recipe')
