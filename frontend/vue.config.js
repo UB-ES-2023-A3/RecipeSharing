2d1428e94dba5a61032ced916a8e9867da0dd23a
Gerard01mm <56315258+Gerard01mm@users.noreply.github.com>
2023-11-10 00:22:26 +0100

    vue.config.js arreglado

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: 'http://localhost:8080',
-    outputDir: '../static/dist',
+    outputDir: '../frontend/dist',
     indexPath: '../../templates/_base_vue.html',
 
     configureWebpack: {
