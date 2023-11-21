9e088d7471d3cca522f4aadac96794affad676af
AdrianGub <agalanpa23@alumnes.ub.edu>
2023-11-21 17:54:16 +0100

    Apply Filters Name

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -213,61 +213,181 @@ export default {
         async applyFilters() {
             await this.fetchData();
             try {
-                let filters = [
-                    {condition: this.selectedTitle !== "", values: this.selectedTitle, prefix: "", param: "title"},
-                    {
-                        condition: this.showRecipesFilterPrepTime,
-                        values: this.selectedPreparationTime,
-                        prefix: "%A2",
-                        param: "preparation_time"
-                    },
-                    {
-                        condition: this.showRecipesFilterServings,
-                        values: this.selectedServings,
-                        prefix: "%A2",
-                        param: "servings"
-                    },
-                    {
-                        condition: this.showRecipesFilterTypes,
-                        values: this.selectedTypes,
-                        prefix: "",
-                        param: "recipe_type"
-                    },
-                    {
-                        condition: this.showRecipesFilterAllergens,
-                        values: this.selectedAllergens,
-                        prefix: "%A2",
-                        param: "allergens"
-                    },
-                    {
-                        condition: this.showRecipesFilterIngredients,
-                        values: this.selectedIngredients,
-                        prefix: "%A2",
-                        param: "ingredients"
-                    }
-                ];
-
+                let prev = false;
                 let endpoint = 'recipes/';
-                let isFirstParam = true;
-
-                filters.forEach(filter => {
-                    if (filter.condition) {
-                        const values = filter.values.length > 1 ? filter.values.join('+') : filter.values[0];
-                        endpoint += isFirstParam ? `${filter.param}=${values}` : `${filter.prefix}${filter.param}=${values}`;
-                        isFirstParam = false;
+                if (this.selectedTitle != "") {
+                    endpoint += `title=${this.selectedTitle}`;
+                    prev = true;
+                }
+                if (this.showRecipesFilterPrepTime) {
+                    if (prev) {
+                        if (this.selectedPreparationTime.length > 1) {
+                            for (let i = 0; i < this.selectedPreparationTime.length; i++) {
+                                if (i == 0) {
+                                    endpoint += `%A2preparation_time=${this.selectedPreparationTime[i]}+`
+                                } else if (i == this.selectedPreparationTime.length - 1) {
+                                    endpoint += `${this.selectedPreparationTime[i]}`
+                                } else {
+                                    endpoint += `${this.selectedPreparationTime[i]}+`
+                                }
+                            }
+                        } else {
+                            endpoint += `%A2preparation_time=${this.selectedPreparationTime[0]}`
+                        }
+                    } else {
+                        if (this.selectedPreparationTime.length > 1) {
+                            for (let i = 0; i < this.selectedPreparationTime.length; i++) {
+                                if (i == 0) {
+                                    endpoint += `preparation_time=${this.selectedPreparationTime[i]}+`
+                                } else if (i == this.selectedPreparationTime.length - 1) {
+                                    endpoint += `${this.selectedPreparationTime[i]}`
+                                } else {
+                                    endpoint += `${this.selectedPreparationTime[i]}+`
+                                }
+                            }
+                        } else {
+                            endpoint += `preparation_time=${this.selectedPreparationTime[0]}`
+                        }
+                        prev = true;
                     }
-                });
-
+                }
+                if (this.showRecipesFilterServings) {
+                    if (prev) {
+                        if (this.selectedServings.length > 1) {
+                            for (let i = 0; i < this.selectedServings.length; i++) {
+                                if (i == 0) {
+                                    endpoint += `%A2servings=${this.selectedServings[i]}+`
+                                } else if (i == this.selectedServings.length - 1) {
+                                    endpoint += `${this.selectedServings[i]}`
+                                } else {
+                                    endpoint += `${this.selectedServings[i]}+`
+                                }
+                            }
+                        } else {
+                            endpoint += `%A2servings=${this.selectedServings[0]}`
+                        }
+                    } else {
+                        if (this.selectedServings.length > 1) {
+                            for (let i = 0; i < this.selectedServings.length; i++) {
+                                if (i == 0) {
+                                    endpoint += `servings=${this.selectedServings[i]}+`
+                                } else if (i == this.selectedServings.length - 1) {
+                                    endpoint += `${this.selectedServings[i]}`
+                                } else {
+                                    endpoint += `${this.selectedServings[i]}+`
+                                }
+                            }
+                        } else {
+                            endpoint += `servings=${this.selectedServings[0]}`
+                        }
+                        prev = true;
+                    }
+                }
+                if (this.showRecipesFilterTypes) {
+                    if (prev) {
+                        if (this.selectedTypes.length > 1) {
+                            for (let i = 0; i < this.selectedTypes.length; i++) {
+                                if (i == 0) {
+                                    endpoint += `%A2recipe_type=${this.selectedTypes[i]}+`
+                                } else if (i == this.selectedTypes.length - 1) {
+                                    endpoint += `${this.selectedTypes[i]}`
+                                } else {
+                                    endpoint += `${this.selectedTypes[i]}+`
+                                }
+                            }
+                        } else {
+                            endpoint += `%A2recipe_type=${this.selectedTypes[0]}`
+                        }
+                    } else {
+                        if (this.selectedTypes.length > 1) {
+                            for (let i = 0; i < this.selectedTypes.length; i++) {
+                                if (i == 0) {
+                                    endpoint += `recipe_type=${this.selectedTypes[i]}+`
+                                } else if (i == this.selectedTypes.length - 1) {
+                                    endpoint += `${this.selectedTypes[i]}`
+                                } else {
+                                    endpoint += `${this.selectedTypes[i]}+`
+                                }
+                            }
+                        } else {
+                            endpoint += `recipe_type=${this.selectedTypes[0]}`
+                        }
+                        prev = true;
+                    }
+                }
+                if (this.showRecipesFilterAllergens) {
+                    if (prev) {
+                        if (this.selectedAllergens.length > 1) {
+                            for (let i = 0; i < this.selectedAllergens.length; i++) {
+                                if (i == 0) {
+                                    endpoint += `%A2allergens=${this.selectedAllergens[i]}+`
+                                } else if (i == this.selectedAllergens.length - 1) {
+                                    endpoint += `${this.selectedAllergens[i]}`
+                                } else {
+                                    endpoint += `${this.selectedAllergens[i]}+`
+                                }
+                            }
+                        } else {
+                            endpoint += `%A2allergens=${this.selectedAllergens[0]}`
+                        }
+                    } else {
+                        if (this.selectedAllergens.length > 1) {
+                            for (let i = 0; i < this.selectedAllergens.length; i++) {
+                                if (i == 0) {
+                                    endpoint += `allergens=${this.selectedAllergens[i]}+`
+                                } else if (i == this.selectedAllergens.length - 1) {
+                                    endpoint += `${this.selectedAllergens[i]}`
+                                } else {
+                                    endpoint += `${this.selectedAllergens[i]}+`
+                                }
+                            }
+                        } else {
+                            endpoint += `allergens=${this.selectedAllergens[0]}`
+                        }
+                        prev = true;
+                    }
+                }
+                if (this.showRecipesFilterIngredients) {
+                    if (prev) {
+                        if (this.selectedIngredients.length > 1) {
+                            for (let i = 0; i < this.selectedIngredients.length; i++) {
+                                if (i == 0) {
+                                    endpoint += `%A2ingredients=${this.selectedIngredients[i]}+`
+                                } else if (i == this.selectedIngredients.length - 1) {
+                                    endpoint += `${this.selectedIngredients[i]}`
+                                } else {
+                                    endpoint += `${this.selectedIngredients[i]}+`
+                                }
+                            }
+                        } else {
+                            endpoint += `%A2ingredients=${this.selectedIngredients[0]}`
+                        }
+                    } else {
+                        if (this.selectedIngredients.length > 1) {
+                            for (let i = 0; i < this.selectedIngredients.length; i++) {
+                                if (i == 0) {
+                                    endpoint += `ingredients=${this.selectedIngredients[i]}+`
+                                } else if (i == this.selectedIngredients.length - 1) {
+                                    endpoint += `${this.selectedIngredients[i]}`
+                                } else {
+                                    endpoint += `${this.selectedIngredients[i]}+`
+                                }
+                            }
+                        } else {
+                            endpoint += `ingredients=${this.selectedIngredients[0]}`
+                        }
+                        prev = true;
+                    }
+                }
                 const response = await axios.get(endpoint);
-
                 if (response.status === 200) {
                     const recipes = response.data.recipes;
                     this.recipesByFilter = recipes;
                     this.showRecipesFilter = true;
-                    console.log(recipes);
+                    console.log(response.data.recipes);
                 }
             } catch (error) {
-                console.error("Error fetching recipes by filters:", error);
+                console.error("Error fetching recipes by name:", error);
             }
         },
         async fetchData() {
