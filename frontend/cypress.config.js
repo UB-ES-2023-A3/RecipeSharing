375688d5268f4bc0c4247e711fa45b649ae9e440
IvanMartinezVilla <ivanmv2000@gmail.com>
2023-11-24 14:21:59 +0100

    cypress

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,12 +1,37 @@
 const { defineConfig } = require("cypress");
 
-module.exports = defineConfig({
-  components: {
-    componentFolder: "frontend/src", // Ruta relativa al directorio raíz donde se encuentran tus componentes
-    testFiles: "**/*.cy.js", // Patrón para buscar archivos de prueba
+// Configuración para pruebas de componentes
+const componentTestingConfig = {
+  component: {
+    componentFolder: "frontend/src",
+    testFiles: "**/*.cy.js",
     devServer: {
       framework: "vue-cli",
       bundler: "webpack",
     },
   },
+};
+
+// Configuración para pruebas e2e
+const e2eTestingConfig = {
+  testFiles: "**/*.e2e.js",
+  // Otras configuraciones para e2e si es necesario
+};
+
+// Configuración por defecto (común a ambas)
+const defaultConfig = {
+  // Otras configuraciones comunes
+};
+
+// Exporta ambas configuraciones
+module.exports = defineConfig({
+  // Combina la configuración por defecto con la configuración específica
+  ...defaultConfig,
+
+  // Configuración específica para pruebas de componentes
+  ...componentTestingConfig,
+
+  // Configuración específica para pruebas e2e
+  // Comenta o descomenta la siguiente línea según necesites ejecutar componentes o e2e
+  // ...e2eTestingConfig,
 });
