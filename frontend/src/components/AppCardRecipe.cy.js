cf720b4335cb251d541586bc07e0e91113180014
IvanMartinezVilla <ivanmv2000@gmail.com>
2023-11-24 12:08:53 +0100

    cypress

---
+++ frontend/src/components/AppCardRecipe.cy.js
@@ -0,0 +1,35 @@
+// cypress/integration/my_component_spec.js
+import AppCardRecipe from "./AppCardRecipe.vue";
+
+describe("<AppCardRecipe />", () => {
+  beforeEach(() => {
+    // Pasa algunas props necesarias
+    const recipe = {
+      id: 1,
+      title: "Test Recipe",
+      rating_amount: 5,
+    };
+    const username = "testUser";
+    const logged = true;
+    const type = "rate";
+    const visibleRecipes = 4;
+
+    // Monta el componente pasando las props necesarias
+    cy.mount(AppCardRecipe, {
+      propsData: {
+        recipe,
+        username,
+        logged,
+        type,
+        visibleRecipes,
+      },
+    });
+  });
+
+  it("renders correctly", () => {
+    // Verifica que el componente se haya rendereado correctamente
+    cy.get(".recipe-card").should("exist");
+  });
+
+  // Otras pruebas...
+});
