8f300ecbe000b5c4542a78c09b39b8a70b0c9a2d
IvanMartinezVilla <ivanmv2000@gmail.com>
2023-11-24 13:59:45 +0100

    cypress

---
+++ frontend/cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  component: {
+    componentFolder: "frontend/src", // Ruta relativa al directorio raíz donde se encuentran tus componentes
+    testFiles: "**/*.cy.js", // Patrón para buscar archivos de prueba
+    devServer: {
+      framework: "vue-cli",
+      bundler: "webpack",
+    },
+  },
+});
