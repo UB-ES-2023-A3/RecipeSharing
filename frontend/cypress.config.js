9c198fa4865e3da5e3a7ba460d2c965b299e0c58
IvanMartinezVilla <ivanmv2000@gmail.com>
2023-11-24 14:43:15 +0100

    cypress

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   component: {
     specPattern: "src/**/*.cy.js",
+    supportFile: false,
     devServer: {
       framework: "vue-cli",
       bundler: "webpack",
