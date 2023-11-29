a5a3f2f3c34582c366855d1958209cff58dc0396
pablonoriega <pablonoriega2002@gmail.com>
2023-11-29 16:31:28 +0100

    float button responsive

--- frontend/src/pages/IngredientsFilterPage.vue
+++ frontend/src/pages/IngredientsFilterPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mainContainer">
-    <h1>Ingredients List A-Z</h1>
+    <h1>Allergens List A-Z</h1>
 
     <!-- Botones de letras -->
     <div class="letrasButtons">
@@ -24,12 +24,24 @@
               v-for="ingrediente in ingredientes"
               :key="ingrediente"
               @click="handleButtonClick(ingrediente)"
-              class="botonIngrediente"
+              :class="{ 'botonIngrediente': true, 'seleccionado': selectedIngredients.includes(ingrediente) }"
             >
               {{ ingrediente }}
             </button>
           </div>
         </div>
+      </div>
+      <div class="floatingContainer" v-if="selectedIngredients.length > 0">
+        <h2>Selected Ingredients</h2>
+        <div class="selected-ingredients-list">
+          <div v-for="selectedIngrediente in selectedIngredients" :key="selectedIngrediente">
+            <span class="selected-item">
+              <span class="ingredient-text">{{ selectedIngrediente }}</span>
+              <span @click="removeIngredient(selectedIngrediente)" class="remove-icon">❌</span>
+            </span>
+          </div>
+        </div>
+        <div class="submit-button" @click="submitSelection">Search</div>
       </div>
     </div>
     <div v-else>
@@ -47,12 +59,20 @@ export default {
       ingredientsData: ingredientsData,
       letras: "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(''),
       letraSeleccionada: null,
+      selectedIngredients: [], // New array to store selected ingredients
     };
   },
   methods: {
     handleButtonClick(ingrediente) {
-      // Manejar el clic en el botón del ingrediente, por ejemplo, podrías mostrar más detalles
-      alert(`Haz clic en ${ingrediente}`);
+      // Toggle the selection state of the clicked ingredient
+      const index = this.selectedIngredients.indexOf(ingrediente);
+      if (index === -1) {
+        // Ingredient not selected, add it to the list
+        this.selectedIngredients.push(ingrediente);
+      } else {
+        // Ingredient selected, remove it from the list
+        this.selectedIngredients.splice(index, 1);
+      }
     },
     scrollToSection(letra) {
       this.letraSeleccionada = letra;
@@ -65,6 +85,16 @@ export default {
     },
     letraDisponible(letra) {
       return Object.keys(this.ingredientsData).includes(letra);
+    },
+    submitSelection() {
+      // Handle the submission of selected ingredients
+      alert('Submitted Ingredients: ' + this.selectedIngredients.join(', '));
+    },
+    removeIngredient(ingredient) {
+      const index = this.selectedIngredients.indexOf(ingredient);
+      if (index !== -1) {
+        this.selectedIngredients.splice(index, 1);
+      }
     },
   },
 };
@@ -108,6 +138,25 @@ h1 {
   background-color: #055480;
 }
 
+.submit-button {
+  text-decoration: none;
+  margin: auto;
+  color: black;
+  background-color: #83d3fc;
+  padding: 5px 10px;
+  border-radius: 2px;
+  cursor: pointer;
+  margin: 5px;
+  transition: background-color 0.3s ease;
+  margin-top: 30px;
+  text-align: center;
+  margin-bottom: 10px;
+}
+
+.submit-button:hover {
+  background-color: #055480;
+}
+
 .letrasButtons a.letraNoDisponible {
   color: #666;
   cursor: not-allowed;
@@ -121,6 +170,8 @@ h1 {
   transition: background-color 0.3s ease;
   margin-top: 40px;
   justify-content: center;
+  margin-right: 200px;
+  margin-left: 200px;
 
 }
 
@@ -132,7 +183,6 @@ h1 {
   border: 2px solid #83d3fc;
 }
 
-
 .tituloSeccion {
   text-align: center;
   font-size: larger;
@@ -140,9 +190,8 @@ h1 {
   width: 25%;
   margin: 0 auto; /* Añadido para centrar horizontalmente */
   padding: 5px;
-    margin-top: 20px;
+  margin-top: 20px;
 }
-
 
 .ingredientesList {
   display: flex;
@@ -169,4 +218,49 @@ h1 {
 .botonIngrediente:hover {
   background-color: #83d3fc;
 }
+
+.botonIngrediente.seleccionado {
+  background-color: #ff5733;
+  color: white;
+}
+
+.floatingContainer {
+  position: fixed;
+  top: 50%;
+  right: 20px;
+  transform: translateY(-50%);
+  background-color: #fff;
+  padding: 20px;
+  border: 1px solid #ccc;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  overflow-y: auto;
+  max-height: 90%; /* Establecer un máximo del 90% del tamaño de la ventana */
+  width: 15%; /* Ancho fijo o puedes usar % para que también sea relativo al tamaño de la ventana */
+  transition: max-height 0.3s ease; /* Agregar una transición para una apariencia suave al cambiar el tamaño */
+}
+
+.selected-ingredients-list {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.selected-item {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 5px;
+}
+
+.ingredient-text {
+  flex-grow: 1; /* Para expandirse y ocupar el espacio restante */
+  margin-right: 10px; /* Espacio entre el texto y la cruz */
+}
+
+.remove-icon {
+  cursor: pointer;
+  color: #ff5733; /* Color para el icono de cruz */
+}
+
+
 </style>
