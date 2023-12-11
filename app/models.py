6a934e3ca5d83f4321f42563e128ece21902d6f9
rubensituuh <72190690+rubensituuh@users.noreply.github.com>
2023-12-12 00:30:12 +0100

    Correcto acceso a los ficheros

--- app/models.py
+++ app/models.py
@@ -23,7 +23,8 @@ class CustomUser(models.Model):
     def save(self, *args, **kwargs):
 
         if self.profile_image is None:
-            with open('default_images/default_profile_image.txt', 'r') as file:
+            with open('app/default_images/default_profile_image.txt',
+                      'r') as file:
                 default_image = file.read()
             self.profile_image = default_image
         super().save(*args, **kwargs)
@@ -89,7 +90,8 @@ class Recipe(models.Model):
     def save(self, *args, **kwargs):
 
         if self.recipe_image is None:
-            with open('default_images/default_recipe_image.txt', 'r') as file:
+            with open('app/default_images/default_recipe_image.txt', 
+                      'r') as file:
                 default_image = file.read()
 
             self.recipe_image = default_image
