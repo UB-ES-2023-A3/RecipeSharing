a2ddfcca2c400a6e22902d4d8fffe93633c5d5be
Gerard01mm <56315258+Gerard01mm@users.noreply.github.com>
2023-12-15 02:02:14 +0100

    corsheaders moved

--- recipeSharing/settings.py
+++ recipeSharing/settings.py
@@ -34,10 +34,10 @@ INSTALLED_APPS = [
 
 # Middleware settings
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
